6c4c3b84448ec22dd439490f9126395ba36425cb
0gravity000 <0gravity000@gmail.com>
2021-08-14 22:15:48 +0900

    Add dashboard view (count market,industry)

--- resources/views/components/stock-index.blade.php
+++ resources/views/components/stock-index.blade.php
@@ -2,22 +2,30 @@
 
 <div class="container">
      <div class="row align-items-start">
-          <div class="col-2">
+          <div class="col-3">
                <ul>
                     <ul>市場</ul>
                     @foreach($uniquedMarketIds as $uniquedMarketId)
+                    @php
+                    $market_count = $stocks->where('market_id', $uniquedMarketId->market_id)->count();
+                    @endphp
                     <li>
                          <a href="/dashboard/show/markets/{{$uniquedMarketId->id}}">
                               {{$uniquedMarketId->market->name}} 
+                              ( {{$market_count}} )
                          </a>
                     </li>
                     @endforeach
                  
                     <ul>業種</ul>
                     @foreach($uniquedIndustryIds as $uniquedIndustryId)
+                    @php
+                    $industry_count = $stocks->where('industry_id', $uniquedIndustryId->industry_id)->count();
+                    @endphp
                     <li>
                          <a href="/dashboard/show/industries/{{$uniquedIndustryId->id}}">
                               {{$uniquedIndustryId->industry->name}}
+                              ( {{$industry_count}} )
                          </a>
                     </li>
                     @endforeach
