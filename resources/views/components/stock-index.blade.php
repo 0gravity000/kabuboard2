7936bb0f48b677bb6b1ad41d4b097dbda0c05242
0gravity000 <0gravity000@gmail.com>
2021-08-14 18:46:36 +0900

    install bootstrap

--- resources/views/components/stock-index.blade.php
+++ resources/views/components/stock-index.blade.php
@@ -1,24 +1,37 @@
 @props(['stocks'])
 
-<table class="table">
-     <thead>
-          <tr>
-          <th scope="col">#</th>
-          <th scope="col">コード</th>
-          <th scope="col">銘柄</th>
-          <th scope="col">市場</th>
-          <th scope="col">業種</th>
-          </tr>
-     </thead>
-     <tbody>
-          @foreach($stocks as $stock)
-          <tr>
-          <th scope="row" >{{$stock->id}}</th>
-          <td>{{$stock->code}}</td>
-          <td>{{$stock->name}}</td>
-          <td>{{$stock->market->name}}</td>
-          <td>{{$stock->industry->name}}</td>
-          </tr>
-          @endforeach
-     </tbody>
-</table>
+<div class="container">
+     <div class="row align-items-start">
+          <div class="col-2">
+               <ul>
+                    <ul>市場</ul>
+                    <li></li>
+                 
+                    <ul>業種</ul>
+                </ul>
+
+          </div>
+          <div class="col">
+               <table class="table">
+                    <thead>
+                         <tr>
+                         <th scope="col">コード</th>
+                         <th scope="col">銘柄</th>
+                         <th scope="col">市場</th>
+                         <th scope="col">業種</th>
+                         </tr>
+                    </thead>
+                    <tbody>
+                         @foreach($stocks as $stock)
+                         <tr>
+                         <td>{{$stock->code}}</td>
+                         <td>{{$stock->name}}</td>
+                         <td>{{$stock->market->name}}</td>
+                         <td>{{$stock->industry->name}}</td>
+                         </tr>
+                         @endforeach
+                    </tbody>
+               </table>
+          </div>
+     </div>
+ </div>
